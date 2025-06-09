78ecf2b5c41acb2536e4020fce0f202ea6573ad4
Bryan Hann <nobody@nowhere>
2025-06-09 14:00:19 -0230

    Use [bch:000:linkall].

--- .bch/activate.sh
+++ .bch/activate.sh
@@ -2,22 +2,17 @@
 
 r=$(dirname $(dirname ${BASH_SOURCE[0]}))
 
-rm -f ~/.emacs.d
-ln -s ${r}/config.emacs ~/.emacs.d
 export BCH_CONFIG_EMACS__root=${r}
 export BCH_CONFIG_EMACS__init=${r}/.bch/activate.sh
 export BCH_CONFIG_EMACS__lbin=${r}/.bch/lbin
 export BCH_CONFIG_EMACS__bin=${r}/.bch/bin
 export BCH_CONFIG_EMACS__lib=${r}/.bch/lib
 
-for s in $(ls ${r}/.bch/lbin/bch*); do
-     d=${HOME}/.local/bin/$(basename $s)
-     [ ! -f ${d} ] && echo "[bch.config-emacs] linking ${s}"
-     [ ! -f ${d} ] && ln -s ${s} ${d}
-done
+rm -f ~/.emacs.d
+ln -s ${r}/config.emacs ~/.emacs.d
+
+bch:000:linkall ${r}/.bch/lbin
 
 source ${r}/.bch/init/init.sh
 
 unset r
-unset s
-unset d
