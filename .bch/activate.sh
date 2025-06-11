5a41dbe7e56f509a972f57b38da707641ed42957
Bryan Hann <nobody@nowhere>
2025-06-11 13:07:26 -0230

    Reduce dependancy on [bch:000:*] functions.

--- .bch/activate.sh
+++ .bch/activate.sh
@@ -2,17 +2,19 @@
 
 r=$(dirname $(dirname ${BASH_SOURCE[0]}))
 
+rm -f ~/.emacs.d
+ln -s ${r}/config.emacs ~/.emacs.d
+
 export BCH_CONFIG_EMACS__root=${r}
 export BCH_CONFIG_EMACS__init=${r}/.bch/activate.sh
 export BCH_CONFIG_EMACS__lbin=${r}/.bch/lbin
 export BCH_CONFIG_EMACS__bin=${r}/.bch/bin
 export BCH_CONFIG_EMACS__lib=${r}/.bch/lib
 
-rm -f ~/.emacs.d
-ln -s ${r}/config.emacs ~/.emacs.d
-
-bch:000:linkall ${r}/.bch/lbin
-
+source ${r}/.bch/init/fn.sh
 source ${r}/.bch/init/init.sh
+
+::lbin:: ${r}/.bch/lbin
+
 
 unset r
